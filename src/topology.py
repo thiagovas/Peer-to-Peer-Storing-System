67fb7da96bff7a38055450e7092a1ba98fe866e5
Carolina Santos Batista <csbatista@icloud.com>
2016-07-04 11:55:05 -0300

    Add files via upload

---
+++ src/topology.py
@@ -0,0 +1,55 @@
+"""Custom topology example
+
+Two directly connected switches plus a host for each switch:
+
+   host --- switch --- switch --- host
+
+Adding the 'topos' dict with a key/value pair to generate our newly defined
+topology enables one to pass in '--topo=mytopo' from the command line.
+"""
+
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+    "Simple topology example."
+
+    def __init__( self ):
+        "Create custom topo."
+        
+        # Initialize topology
+        Topo.__init__( self )
+        
+        # Add hosts and switches
+        cliente = self.addHost( 'h1' )
+        serv1 = self.addHost( 'h2' )
+        serv2 = self.addHost( 'h3' )
+        serv3 = self.addHost( 'h4' )
+        serv4 = self.addHost( 'h5' )
+        serv5 = self.addHost( 'h6' )
+
+        switch1 = self.addSwitch( 's1' )
+        switch2 = self.addSwitch( 's2' )
+        switch3 = self.addSwitch( 's3' )
+        switch4 = self.addSwitch( 's4' )
+        switch5 = self.addSwitch( 's5' )
+        
+        # Add links
+        self.addLink(cliente, switch1)
+        self.addLink(switch1, serv1)
+
+        self.addLink(serv1, switch2)
+        self.addLink(switch2, serv2)
+
+        self.addLink(serv2, switch3)
+        self.addLink(switch3, serv3)
+
+        self.addLink(serv3, switch4)
+        self.addLink(switch4, serv4)
+
+        self.addLink(serv4, switch5)
+        self.addLink(switch5, serv5)
+
+
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
